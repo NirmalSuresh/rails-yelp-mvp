9d006797755f5a23747f5e53a6683d8d6be1a8a7
Nirmal Suresh <nirmalsuresh16@gmail.com>
2025-11-19 07:14:35 +0000

    changes made

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+Restaurant.create!([
+  { name: "Dominos", address: "MG Road", phone_number: "999999", category: "italian" },
+  { name: "Empire", address: "Brigade Road", phone_number: "888888", category: "japanese" },
+  { name: "BBQ Nation", address: "Indiranagar", phone_number: "777777", category: "french" },
+  { name: "Meghana", address: "Koramangala", phone_number: "666666", category: "chinese" },
+  { name: "Truffles", address: "HSR", phone_number: "555555", category: "belgian" }
+])
